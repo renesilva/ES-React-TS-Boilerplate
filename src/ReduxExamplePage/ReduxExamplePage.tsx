796cf0f08c09ebd06670276cba554d86f33c65a5
Eressea RS <rsilva@eresseasolutions.com>
2022-09-18 19:37:24 -0400

    Redux example. Added by 3

--- src/ReduxExamplePage/ReduxExamplePage.tsx
+++ src/ReduxExamplePage/ReduxExamplePage.tsx
@@ -1,20 +1,27 @@
 import { useAppDispatch, useAppSelector } from '../redux/app/hooks';
-import { incremented } from '../redux/features/counter/counter-slice';
+import { incremented, amountAdded } from '../redux/features/counter/counter-slice';
 
 export default function ReduxExamplePage() {
   // redux
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
   // handlers
-  const handleClickCount = () => {
+  const handleClickAddBy1 = () => {
     dispatch(incremented());
+  };
+  const handleClickAddByX = () => {
+    dispatch(amountAdded(3));
   };
   // render
   return (
     <div className="container">
       <h2>Hello</h2>
-      <button onClick={handleClickCount} className="btn btn-primary">
-        count is {count}
+      <p>count is {count}</p>
+      <button onClick={handleClickAddBy1} className="btn btn-primary">
+        Add by 1
+      </button>{' '}
+      <button onClick={handleClickAddByX} className="btn btn-primary">
+        Add by 3
       </button>
     </div>
   );
--- src/redux/features/counter/counter-slice.ts
+++ src/redux/features/counter/counter-slice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'; // PayloadAction
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface CounterState {
   value: number;
@@ -15,8 +15,11 @@ const counterSlice = createSlice({
     incremented(state) {
       state.value++;
     },
+    amountAdded(state, action: PayloadAction<number>) {
+      state.value += action.payload;
+    },
   },
 });
 
-export const { incremented } = counterSlice.actions;
+export const { incremented, amountAdded } = counterSlice.actions;
 export default counterSlice.reducer;

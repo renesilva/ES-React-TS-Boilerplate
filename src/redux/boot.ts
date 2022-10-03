9d6b1a29dfdde85123af22eec0981b55afc44bd7
Eressea RS <rsilva@eresseasolutions.com>
2022-10-03 08:45:43 -0400

    Boot type

--- src/redux/boot.ts
+++ src/redux/boot.ts
@@ -1,7 +1,7 @@
 import { store } from './app/store';
 import { checkAuthorization } from './features/auth/auth-slice';
 
-export default function Boot() {
+export default function Boot(): Promise<any> {
   return new Promise(() => {
     store.dispatch(checkAuthorization());
   });

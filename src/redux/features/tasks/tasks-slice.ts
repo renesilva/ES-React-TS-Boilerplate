d0f5919e566a36b683c07baf31825907d1c1c6d0
Eressea RS <rsilva@eresseasolutions.com>
2022-10-03 10:42:29 -0400

    Tasks Redux with save to LocalStorage

--- src/redux/features/tasks/tasks-slice.ts
+++ src/redux/features/tasks/tasks-slice.ts
@@ -1,12 +1,13 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { iTask } from '../../../components/Examples/TodoList/iTask';
+import { storageGet, storageSave } from '../../../services/Storage/Storage';
 
 interface TasksState {
   tasks: iTask[];
 }
 
 const initialState: TasksState = {
-  tasks: [],
+  tasks: storageGet('tasks', []),
 };
 
 const tasksSlice = createSlice({
@@ -15,12 +16,15 @@ const tasksSlice = createSlice({
   reducers: {
     addTask(state, action: PayloadAction<iTask>) {
       state.tasks.push(action.payload);
+      storageSave('tasks', state.tasks);
     },
     removeTask(state, action: PayloadAction<number>) {
       state.tasks.splice(action.payload, 1);
+      storageSave('tasks', state.tasks);
     },
     markTaskDone(state, action: PayloadAction<number>) {
       state.tasks[action.payload].done = !state.tasks[action.payload].done;
+      storageSave('tasks', state.tasks);
     },
   },
 });
